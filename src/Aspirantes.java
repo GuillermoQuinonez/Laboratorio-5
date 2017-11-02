0e7b946f2f958fe422cd1ad44fa480c8c3c9040e
Margarita Quiñonez <Margarita Quionez@DESKTOP-B68LRTV>
2017-11-02 07:51:03 -0600

    Se hizó el contructor de Aspirantes

--- src/Aspirantes.java
+++ src/Aspirantes.java
@@ -1,4 +1,15 @@
 
-public class Aspirantes {
+public abstract class Aspirantes {
+	protected String nombre; 
+	protected String dpi; 
+	protected float notaEscalafon; 
+	protected float notaHistoria; 
+	
+	public Aspirantes(String nombre, String dpi, float notaEscalafon, float notaHistoria) {
+		this.nombre = nombre; 
+		this.dpi = dpi; 
+		this.notaEscalafon = notaEscalafon;
+		this.notaHistoria = notaHistoria; 
+	}
 
 }
