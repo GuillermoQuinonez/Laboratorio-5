f70e66ebab086aec84c7d94dad32770fb4b00fbe
Margarita Quiñonez <Margarita Quionez@DESKTOP-B68LRTV>
2017-11-02 08:27:28 -0600

    Se creó clase EgresadoSecundaria

---
+++ src/EgresadoSecundaria.java
@@ -0,0 +1,9 @@
+/**
+ * @file Aspirantes.java
+ * @author José Guillermo Quiñónez Castillo <qui17775@uvg.edu.gt>
+ * @author Estuardo 
+ * @version 2/11/2017 A
+ */
+public class EgresadoSecundaria {
+
+}
