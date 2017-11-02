66709de3cb919c48b4e4665ccc82c969b2904559
Margarita Quiñonez <Margarita Quionez@DESKTOP-B68LRTV>
2017-11-02 08:12:37 -0600

    Clase Aspirantes terminada

--- src/Aspirantes.java
+++ src/Aspirantes.java
@@ -1,5 +1,10 @@
-
-public abstract class Aspirantes {
+/**
+ * @file Aspirantes.java
+ * @author José Guillermo Quiñónez Castillo <qui17775@uvg.edu.gt>
+ * @author Estuardo 
+ * @version 2/11/2017 A
+ */
+public abstract class Aspirantes implements Comparable<Aspirantes>{
 	protected String nombre; 
 	protected String dpi; 
 	protected float notaEscalafon; 
@@ -11,5 +16,93 @@ public abstract class Aspirantes {
 		this.notaEscalafon = notaEscalafon;
 		this.notaHistoria = notaHistoria; 
 	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the dpi
+	 */
+	public String getDpi() {
+		return dpi;
+	}
+
+	/**
+	 * @param dpi the dpi to set
+	 */
+	public void setDpi(String dpi) {
+		this.dpi = dpi;
+	}
+
+	/**
+	 * @return the notaEscalafon
+	 */
+	public float getNotaEscalafon() {
+		return notaEscalafon;
+	}
+
+	/**
+	 * @param notaEscalafon the notaEscalafon to set
+	 */
+	public void setNotaEscalafon(float notaEscalafon) {
+		this.notaEscalafon = notaEscalafon;
+	}
+
+	/**
+	 * @return the notaHistoria
+	 */
+	public float getNotaHistoria() {
+		return notaHistoria;
+	}
+
+	/**
+	 * @param notaHistoria the notaHistoria to set
+	 */
+	public void setNotaHistoria(float notaHistoria) {
+		this.notaHistoria = notaHistoria;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
+	@Override
+	public String toString() {
+		return "Aspirantes [nombre=" + nombre + ", dpi=" + dpi + ", notaEscalafon=" + notaEscalafon + ", notaHistoria="
+				+ notaHistoria + "]";
+	}
+	
+	/* (non-Javadoc)
+	 * @see java.lang.Comparable#compareTo(java.lang.Object)
+	 */
+	@Override
+	public int compareTo(Aspirantes a) {
+		if(notaEscalafon < a.getNotaEscalafon()) {
+			return -1; 
+		} 
+		if(notaEscalafon > a.getNotaEscalafon()) {
+			return 1; 
+		}
+		return 0; 
+	}
+	
+	/**
+	 * Calcula la nota para el escalafon según las especificaciones de cada tipo de aspirante
+	 */
+	public abstract void NotaParaEscalafon(); 
+	
+	
+	
+	
 
 }
